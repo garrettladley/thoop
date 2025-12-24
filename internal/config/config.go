f0f32df47005620055f47587c6a72172c12c00cc
Garrett Mitchell Ladley <garrett.ladley14@gmail.com>
2025-12-23 22:13:30 -0500

    feat(dx): run proxy flow locally

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,13 +3,14 @@ package config
 import "github.com/caarlos0/env/v11"
 
 type Config struct {
-	Whoop Whoop `envPrefix:"WHOOP_"`
+	ProxyURL string `env:"PROXY_URL,required"`
+	Whoop    Whoop  `envPrefix:"WHOOP_"`
 }
 
 type Whoop struct {
-	ClientID     string `env:"CLIENT_ID"`
-	ClientSecret string `env:"CLIENT_SECRET"`
-	RedirectURL  string `env:"REDIRECT_URL"`
+	ClientID     string `env:"CLIENT_ID,required"`
+	ClientSecret string `env:"CLIENT_SECRET,required"`
+	RedirectURL  string `env:"REDIRECT_URL,required"`
 }
 
 func Read() (Config, error) {
