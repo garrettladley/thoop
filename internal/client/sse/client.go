3085f3a9c7a8ea28766d3f575d3b45684c2dabbb
Garrett Ladley <92384606+garrettladley@users.noreply.github.com>
2026-01-07 21:13:18 -0500

    nit(xhttp): `SetRequestHeaderAPIKey` (#96)

--- internal/client/sse/client.go
+++ internal/client/sse/client.go
@@ -230,7 +230,7 @@ func (t *sseTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 		xhttp.SetRequestHeaderSessionID(req, t.sessionID)
 	}
 	if t.apiKey != "" {
-		req.Header.Set(xhttp.XAPIKey, t.apiKey)
+		xhttp.SetRequestHeaderAPIKey(req, t.apiKey)
 	}
 
 	resp, err := t.base.RoundTrip(req)
