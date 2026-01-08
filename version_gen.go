b0e6052a830eb40e2c6f150ae24e170e0c595e36
Garrett Ladley <92384606+garrettladley@users.noreply.github.com>
2026-01-07 22:22:49 -0500

    chore(main): release 0.0.12 (#102)

--- version_gen.go
+++ version_gen.go
@@ -5,6 +5,6 @@ package thoop
 const (
 	VersionMajor uint   = 0        // x-release-please-major
 	VersionMinor uint   = 0        // x-release-please-minor
-	VersionPatch uint   = 11       // x-release-please-patch
-	Version      string = "0.0.11" // x-release-please-version
+	VersionPatch uint   = 12       // x-release-please-patch
+	Version      string = "0.0.12" // x-release-please-version
 )
